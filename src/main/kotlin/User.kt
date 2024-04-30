20ffd4e88da2d28f1a011ef0e640f3a9dfb60662
AWKRID <minzoo202738@gmail.com>
2024-04-30 15:09:04 +0900

    :sparkles: feat: User 클래스 생성

---
+++ src/main/kotlin/User.kt
@@ -0,0 +1,25 @@
+package org.example
+
+class User {
+    private var yPos = 0
+    private var xPos = 0
+    private var coin = 0
+    fun moveUp() {
+        if (yPos > 0) yPos--
+    }
+
+    fun moveDown() {
+        if (yPos < 4) yPos++
+    }
+
+    fun moveLeft() {
+        if (xPos > 0) xPos--
+    }
+
+    fun moveRight() {
+        if (xPos < 4) xPos++
+    }
+    fun increaseCoin(rewards : Int) {
+        coin += rewards
+    }
+}
